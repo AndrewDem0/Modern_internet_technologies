c0c41f3a9d8bc252d2aa250957357bba4cf167e0
Marino <demedp.s@gmail.com>
2025-11-19 15:11:31 +0200

    ModelState.IsValid usage

---
+++ WebApplication/Controllers/OrderController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication.ViewModels;
+namespace WebApplication.Controllers
+{
+    public class OrderController : Controller
+    {
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(OrderViewModel model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                return Content($"Замовлення прийнято! Товар: {model.ProductName}, Кількість: {model.Quantity}");
+            }
+
+            return View(model);
+        }
+    }
+}
