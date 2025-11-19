7541575849f348dd68bc41f65fe85ef6325d80e6
Marino <demedp.s@gmail.com>
2025-11-19 15:07:12 +0200

    Added 5 validation standarts | Task 2

---
+++ WebApplication/ViewModels/OrderViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication.ViewModels
+{
+    public class OrderViewModel
+    {
+
+        [Required(ErrorMessage = "Назва товару є обов'язковою.")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Назва товару має бути від 3 до 50 символів.")]
+        [Display(Name = "Назва товару")]
+        public string ProductName { get; set; }
+
+        [Required]
+        [Range(1, 100, ErrorMessage = "Кількість має бути від 1 до 100.")]
+        [Display(Name = "Кількість")]
+        public int Quantity { get; set; }
+
+        [Required]
+        [Range(0.01, 10000.00, ErrorMessage = "Ціна повинна бути більше 0.")]
+        [Display(Name = "Ціна за одиницю")]
+        public decimal Price { get; set; }
+
+        [Required]
+        [EmailAddress(ErrorMessage = "Некоректний формат Email.")]
+        [Display(Name = "Email замовника")]
+        public string CustomerEmail { get; set; }
+
+        [Required]
+        [Compare("CustomerEmail", ErrorMessage = "Email адреси не співпадають.")]
+        [Display(Name = "Підтвердіть Email")]
+        public string ConfirmCustomerEmail { get; set; }
+    }
+}
